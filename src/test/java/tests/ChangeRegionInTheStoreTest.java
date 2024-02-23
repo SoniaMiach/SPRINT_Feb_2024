714f28b9516529542d39745a491f834de9823495
Goldbe54 <dimon54royko@gmail.com>
2024-02-23 17:06:33 +0200

    Cheak the change of region

---
+++ src/test/java/tests/ChangeRegionInTheStoreTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import common.TestInit;
+import io.qameta.allure.Description;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.NovusPage;
+
+import static com.codeborne.selenide.Selenide.switchTo;
+
+public class ChangeRegionInTheStoreTest extends TestInit {
+
+    HomePage homePage = new HomePage();
+    NovusPage novusPage = new NovusPage();
+
+    @DataProvider(name = "expectedRegionTexts")
+    public Object[][] provideExpectedRegionTexts() {
+        return new Object[][]{
+                {"Київ"},
+                {"Рівне"},
+        };
+    }
+
+    @Test(description = "Change the region in the store", dataProvider = "expectedRegionTexts")
+    @Description("SF2-25")
+    public void changeRegion(String expectedSubstring) {
+        homePage.clickMarketIcon("NOVUS");
+        switchTo().window(1);
+        novusPage.closePopCart();
+        novusPage.clickChangeRegionButton();
+        novusPage.clickSelfPickupButton();
+        novusPage.insertRegionField(expectedSubstring);
+
+        String regionText = novusPage.getTextRegion();
+
+        softAssert.assertTrue(regionText.contains(expectedSubstring),
+                "The text does not match " + expectedSubstring);
+    }
+}
